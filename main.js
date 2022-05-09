ca22f86df5835270675b1dabdaaa0f68618461bd
ani100 <85432450+ani100@users.noreply.github.com>
2022-05-09 17:51:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() {
+
+}
+
+function setup() {
+
+    canvas = createCanvas(300,300);
+    canvas.center();
+        
+}
+
+function draw() {
+
+}
+
+function take_snap() {
+
+    save('My_Filter_Image_Pic.png')
+
+}
